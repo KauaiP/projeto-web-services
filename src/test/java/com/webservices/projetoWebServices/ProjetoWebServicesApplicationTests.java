2091ee95bbdea7b8b1d374552084d438fdbdffdc
KauaiP <kauaipereira@id.uff.br>
2026-02-11 14:55:06 -0300

    Project created

---
+++ src/test/java/com/webservices/projetoWebServices/ProjetoWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservices.projetoWebServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
